dc82c36b3447a68a8591a47f9a7cecad4754667d
dhanushreddy291 <29dhanushreddy@gmail.com>
2022-02-07 05:23:39 +0000

    Adding Files

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret
 auth.set_access_token(os.getenv('access_token'), os.getenv('access_secret'))
 api = tweepy.API(auth)
 
-api.update_status("Tweeting, using my VSCode")
+media = api.media_upload("CuteCat.gif")
+tweet=""
+if api.update_status(status=tweet, media_ids=[media.media_id]) is not None:
+    print("Successfully Tweeted")
