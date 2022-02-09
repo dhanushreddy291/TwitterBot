7e4beddd9f72702cbeb6ce6a62efdb285e5fc701
dhanushreddy291 <29dhanushreddy@gmail.com>
2022-02-09 17:38:19 +0000

    Updating Files

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 from datetime import datetime
 load_dotenv()
 
-auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret'))
-auth.set_access_token(os.getenv('access_token'), os.getenv('access_secret'))
-api = tweepy.API(auth)
+def cronjob():
+    auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret'))
+    auth.set_access_token(os.getenv('access_token'), os.getenv('access_secret'))
+    api = tweepy.API(auth)
 
-urllib.request.urlretrieve("https://cataas.com/cat/gif", "CuteCat.gif")
-media = api.media_upload("CuteCat.gif")
-if api.update_status(status="", media_ids=[media.media_id]) is not None:
-    print("Tweet was successful " + datetime.now().strftime("%H:%M:%S"))
-else:
-    print("Tweet was unsuccessful")
+    urllib.request.urlretrieve("https://cataas.com/cat/gif", "CuteCat.gif")
+    media = api.media_upload("CuteCat.gif")
+    if api.update_status(status="", media_ids=[media.media_id]) is not None:
+        print("Tweet was successful " + datetime.now().strftime("%H:%M:%S"))
+    else:
+        print("Tweet was unsuccessful")
