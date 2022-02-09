b7683ded242e7203ea6ef7190f9297bd313092c4
dhanushreddy291 <29dhanushreddy@gmail.com>
2022-02-09 17:49:04 +0000

    Updating Files

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ from main import cronjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, "interval", seconds=120)
+scheduler.add_job(cronjob, "interval", seconds=1200)
 
 scheduler.start()
