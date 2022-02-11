54214fe083695be41bdbe85c6ad225eab04728c8
dhanushreddy291 <76517652+dhanushreddy291@users.noreply.github.com>
2022-02-11 16:09:24 +0530

    Finalized Version

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from flask import Flask
 import tweepy, os, urllib.request
 from PIL import Image
 from dotenv import load_dotenv
 from datetime import datetime
-from flask import Flask
 load_dotenv()
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret'))
     auth.set_access_token(os.getenv('access_token'), os.getenv('access_secret'))
@@ -17,5 +17,3 @@ def index():
         return "The, Tweet was successful " + datetime.now().strftime("%H:%M:%S")
     else:
         return "Tweet was not successful"
-
-app.run(host='0.0.0.0', port=81)
